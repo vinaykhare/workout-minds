1a26d62066a62e5f96cb35c509857afd0aa1996f
vinaykhare <vinaykhare@live.in>
2026-03-11 14:59:47 +0530

    Sprint 1 API Key was accidentally pushed.

---
+++ lib/core/constants.dart
@@ -0,0 +1,9 @@
+// lib/core/constants.dart
+
+class AppConstants {
+  // Replace with your actual Gemini API Key from Google AI Studio
+  static const String geminiApiKey = 'Gemini API Key';
+
+  // You can also add Firebase project IDs or other sensitive strings here
+  static const String modelName = 'gemini-2.5-flash-lite';
+}
